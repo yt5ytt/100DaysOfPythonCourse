4498708bccdb4510d06840adaea8533ac7327376
yt5ytt <yt5ytt@gmail.com>
2021-12-10 12:38:26 +0100

    NumberGuessingGame

---
+++ Day12Challenge/functions.py
@@ -0,0 +1,42 @@
+import random
+
+def clear():
+    """ Clears screen before the game starts"""
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
+
+def level(difficulty):
+    if difficulty == 'easy':
+        tryes = 10
+    elif difficulty == 'hard':
+        tryes = 5
+    else:
+        tryes = 0
+    
+    return tryes
+
+def ask_difficulty():
+    difficulty = input("Izaberi tezinu. Ukucaj 'easy' ako hoces da imas 10 pokusaja ili da probas sa 'hard' ako mislis da mozes da pogodis u 5 pokusaja: ").lower()
+    num_of_tryes = level(difficulty)
+    if difficulty == 'easy':
+        print("\nIzabrao si laksi nivo. Imas 10 pokusaja\n")
+    elif difficulty == 'hard':
+        print("\nIzabrao si tezi nivo. Imas samo 5 pokusaja\n")
+    else:
+        print("\nUkucao si nepostojecu opciju. Pokusaj ponovo\n")
+        ask_difficulty()
+    
+    return num_of_tryes
+
+def random_number():
+    number = random.randint(1, 100)
+    return number
+
+def check(guess, random_number):
+    if guess == random_number:
+      print("BRAVO... POGODIO SI... POBEDA!!!\n")
+    elif guess <= random_number:
+      print("Malo je... Broj je veci!\nPogadjaj ponovo.\n")
+    elif guess >= random_number:
+      print("Mnogo je kume... Smanji malo!!!\nPogadjaj ponovo.\n")
---
+++ Day12Challenge/main.py
@@ -0,0 +1,35 @@
+from random import random
+import functions
+
+play = True
+while play:
+  functions.clear()
+  from logo import logo
+  print(logo)
+  print("Dobro dosli u igru 'Pogodi broj'")
+  print("Zamislicu broj izmedju 1 i 100.")
+  
+  num_of_tryes = functions.ask_difficulty()
+
+  random_number = functions.random_number()
+
+  for atempt in range(num_of_tryes):
+    guess = int(input(f"Pokusaj broj {atempt + 1}: "))
+    functions.check(guess, random_number)    
+    
+    if guess == random_number:
+      break
+    # elif guess <= random_number:
+    #   print("Malo je... Broj je veci!\nPogadjaj ponovo.\n")
+    # elif guess >= random_number:
+    #   print("Mnogo je kume... Smanji malo!!!\nPogadjaj ponovo.\n")
+    
+  if guess != random_number:
+    print("Nemas vise pravo da pogadjas. IZGUBIO SI!!!\n")  
+
+  play_again = input("Da li zelis ponovo da igras igru. Ukucaj 'y' ako zelis ili 'n' ako ne zelis: ").lower()
+  if play_again == 'n':
+    play = False
+
+
+###End of code
