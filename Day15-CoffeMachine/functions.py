b705f42ff9f03afd729f88b1f24d2fdcd3e1d1bc
yt5ytt <yt5ytt@gmail.com>
2021-12-11 22:50:04 +0100

    CoffeMachine

---
+++ Day15-CoffeMachine/functions.py
@@ -0,0 +1,5 @@
+def clear():
+    """ Clears screen before the game starts"""
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
---
+++ Day15-CoffeMachine/main.py
@@ -0,0 +1,4 @@
+from logo import logo
+from functions import clear
+clear()
+print(logo)
