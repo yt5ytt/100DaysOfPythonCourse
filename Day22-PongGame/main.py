e2851e97d81c1eeee3093c6f5eac8d0f9c562876
yt5ytt <yt5ytt@gmail.com>
2021-12-26 22:34:06 +0100

    PongGame

--- Day22-PongGame/main.py
+++ Day22-PongGame/main.py
@@ -1,11 +1,20 @@
+from turtle import Screen
 from middleLine import MiddleLine
 from screen_setup import ScreenSetup
+from paddles import Paddles
 
 screen = ScreenSetup()
+paddle_move = Screen()
 
 X_FACTOR = int(screen.x_factor())
 Y_FACTOR = int(screen.y_factor())
 
 line = MiddleLine(Y_FACTOR)
+
+paddle = Paddles(X_FACTOR, Y_FACTOR)
+
+paddle.left_paddle()
+paddle.right_paddle()
+
 
 screen.exitonclick()
