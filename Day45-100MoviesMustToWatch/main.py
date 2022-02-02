2ef25058f2d609c79d4ab338c717f88b805ad419
yt5ytt <yt5ytt@gmail.com>
2022-02-03 00:14:34 +0100

    Day45

--- Day45-100MoviesMustToWatch/main.py
+++ Day45-100MoviesMustToWatch/main.py
@@ -11,12 +11,10 @@ soup = BeautifulSoup(web_page, "html.parser")
 titles = soup.select(selector=".listicle-item img")
 
 movies_list = [title.get("alt") for title in titles if title.get("alt") != "Amazon"]
-index = len(movies_list)
+movies_list = movies_list[::-1]
 
-number = 0
-while index > 0:
-    number += 1
+number = 1
+while number <= 100:
     with open("movies_list.txt", "a") as f:
-        f.write(f"{number}) {movies_list[index-1]}\n")
-    print(f"{number}) {movies_list[index-1]}")
-    index -= 1
+        f.write(f"{number}) {movies_list[number-1]}\n")
+    number += 1
