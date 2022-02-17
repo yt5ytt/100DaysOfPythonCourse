8541cd29de7b4289e93475e49acd4b68e01236c4
yt5ytt <yt5ytt@gmail.com>
2022-02-17 13:59:56 +0100

    Day48

--- Day48-CookieGameAuto/interaction.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from main import chrome_driver_path
-
-driver = webdriver.Chrome(chrome_driver_path)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
-
-statistics = driver.find_element_by_xpath('//*[@id="articlecount"]/a[1]')
-statistics.click()
-
--- Day48-CookieGameAuto/main.py
+++ Day48-CookieGameAuto/main.py
@@ -17,15 +17,15 @@ while True:
     cookie.click()
 
     #Every 5 seconds:
-    if time.time() > timeout:
+    # if time.time() > timeout:
 
-        products = driver.find_elements_by_css_selector("#products div.enabled")
-        if len(products) > 0:
-            products[len(products) - 1].click()
+    #     products = driver.find_elements_by_css_selector("#products div.enabled")
+    #     if len(products) > 0:
+    #         products[len(products) - 1].click()
         
-        upgrades = driver.find_elements_by_css_selector("#upgrades div.enabled")
-        if len(upgrades) > 0:
-            upgrades[len(upgrades) - 1].click()
+    #     upgrades = driver.find_elements_by_css_selector("#upgrades div.enabled")
+    #     if len(upgrades) > 0:
+    #         upgrades[len(upgrades) - 1].click()
         
-        #Add another 5 seconds until the next check
-        timeout = time.time() + 5
+    #     #Add another 5 seconds until the next check
+    #     timeout = time.time() + 5
