6e1765d011884f739790edaba1c3514f76b5ebee
yt5ytt <yt5ytt@gmail.com>
2022-03-08 22:57:25 +0100

    Day54

--- Day54-Flask/main.py
+++ Day54-Flask/main.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Da li ce samo da se refreshuje ili mora da se ponovo pokrece server?'
+
+@app.route("/exit")
+def exit_site():
+    return "Ovo je izlaz sa sajta. Samo da probam"
+
+if __name__ == "__main__":
+    app.run()
