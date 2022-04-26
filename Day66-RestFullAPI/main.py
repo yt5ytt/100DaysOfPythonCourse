278c4f2ec0f33ceca4934f065690e2ad6f08846a
yt5ytt <yt5ytt@gmail.com>
2022-04-26 09:43:21 +0200

    Day66

--- Day66-RestFullAPI/main.py
+++ Day66-RestFullAPI/main.py
@@ -5,13 +5,13 @@ from random import choice
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -31,7 +31,7 @@ def home():
     return render_template("index.html")
     
 
-## HTTP GET - Read Record
+# HTTP GET - Read Record
 @app.route("/random", methods=["GET"])
 def random():
     all_cafes = Cafe.query.all()
@@ -50,6 +50,7 @@ def random():
         "seats": random_row.seats
     }
     return jsonify(cafe=cafe)
+
 
 @app.route("/all", methods=["GET"])
 def all():
@@ -72,6 +73,7 @@ def all():
         cafe_list.append(single_cafe)
         
     return jsonify(cafes=cafe_list)
+
 
 @app.route("/search", methods=["GET"])
 def search():
@@ -104,14 +106,70 @@ def search():
         }
         return jsonify(error=error_message)
 
-## HTTP POST - Create Record
-@app.route("/add")
+
+#  HTTP POST - Create Record
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    new_cafe = Cafe(
+        name=request.args.get("name"),
+        map_url=request.args.get("map_url"),
+        img_url=request.args.get("img_url"),
+        location=request.args.get("loc"),
+        has_sockets=bool(request.args.get("sockets")),
+        has_toilet=bool(request.args.get("toilet")),
+        has_wifi=bool(request.args.get("wifi")),
+        can_take_calls=bool(request.args.get("calls")),
+        seats=request.args.get("seats"),
+        coffee_price=request.args.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    message = {
+        "success": "Successfully added a new cafe"
+    }
+    return jsonify(response=message)
 
-## HTTP PUT/PATCH - Update Record
 
-## HTTP DELETE - Delete Record
+# HTTP PUT/PATCH - Update Record
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def update_price(cafe_id):
+    price = request.args.get("new_price")
+    cafe = Cafe.query.get(cafe_id)
+    if cafe:
+        cafe.coffee_price = price
+        db.session.commit()
+        return jsonify(success="Successfully updated the price.")
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
+
+@app.route("/update-seats/<int:cafe_id>", methods=["PATCH"])
+def update_seats(cafe_id):
+    seats = request.args.get("seats_number")
+    cafe = Cafe.query.get(cafe_id)
+    if cafe:
+        cafe.seats = seats
+        db.session.commit()
+        return jsonify(success="Successfully updated number of seats.")
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
+
+# HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete(cafe_id):
+    default_api_key = "TopSecretAPIKey"
+    api_key = request.args.get("api_key")
+    if api_key == default_api_key:
+        cafe = Cafe.query.get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(success="Successfully deleted cafe.")
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+    else:
+        return jsonify(error="Sorry, thats not allowed. Make sure you have the correct api_key.")
 
 
 if __name__ == '__main__':
